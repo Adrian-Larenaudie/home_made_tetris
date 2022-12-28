d131f9bd7985d77b13dfef6eb44265f8cd56ed3b
Adrian Larenaudie <97042742+Adrian-Larenaudie@users.noreply.github.com>
2022-12-28 16:01:32 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -96,20 +96,20 @@ export const game = {
 
 //* LET'S GOO!
 game.init();
-// cet event est appelé qu'une seul fois et détectera le changement de page pour activer une pause si le jeu est lancé
+// cet event est appelé une fois et détectera le changement de page pour activer une pause si le jeu est lancé
 user_input.on_visibility_change_page_event_handler();
 
 /* 
 *DOCUMENTATION
 
-game.js est le fichier principale du jeu.
+game.js est le fichier principal.
 
 On retrouve une partie importation des différents objets contenus dans différents fichiers:
 - user_input.js permet la gestion des inputs utilisateur
-- grid.js permet de générer les positions de la dessiner et de placer une pièce arrivée en bout de parcourt
-- piece.js permet de générer une pièce aléatoirement et de la dessiner puis de la redessiner tout au long de son parcourt
+- grid.js permet de générer les positions de la dessiner et de placer une pièce arrivée en bout de parcours
+- piece.js permet de générer une pièce aléatoirement et de la dessiner puis de la redessiner tout au long de son parcours
 - data.js regroupe un certain nombre de données spécifiques pour faciliter leur pilotage
-- scoring.js qui nous permet de jouer un son lors du croquage de pomme, d'activer désactiver ce son à l'aide d'un bouton sur le document
+- scoring.js permet la gestion du score de la partie courante et l'interaction avec le local storage pour stocker, update et ou récupérer le meilleure score
 TODO - song.JS permet la gestion du son (fonctionnalité à faire)
 
 Ensuite nous avons la partie qui contient notre objet game dans lequel on retrouve plusieurs propriétés et méthodes.
@@ -118,12 +118,12 @@ Les propriétés vont nous permettre de déterminer un état et les méthodes de
 
 *DESCRIPTION DES PROPRIETES: (8)
 
-- running: booléen qui détermine si la partie estr en cours à (true) ou non à (false)
+- running: booléen qui détermine si la partie est en cours à (true) ou non à (false)
 - over,  booléen qui détermine si la partie est terminée ou non
-- interval: ui va recevoir le setInterval du jeu (permet de gérer le parcourt d'une pièce jusqu'à son positionnement)
-- speed: définit la vitesse de déplacement de la pièce (du set interval)
+- interval: qui va recevoir le setInterval du jeu (permet de gérer le parcous d'une pièce jusqu'à son positionnement)
+- speed: définit la vitesse de déplacement de la pièce (set interval)
 - end_positions: regroupe les positions de la dernière ligne de la grille
-- button: reçoit l'élément du DOM qui permet de lancé la partie lors du clique sur ce dernie
+- button: reçoit l'élément du DOM qui permet de lancé la partie lors du clique sur cet élément
 - modal: reçoit l'élément du DOM qui contient la boite modale affiché en début et fin de partie
 - pause_modal: reçoit l'élément du DOM qui contient la boite modale affichée lors d'une pause
 - pause: booléen qui définit si la pause est activée ou non
@@ -133,26 +133,26 @@ Les propriétés vont nous permettre de déterminer un état et les méthodes de
 - init() permet d'initialiser une partie, on va y appeler plusieurs méthodes et effectuer différentes actions:
     - running est passé à false la partie n'est pas lancée
     - initialisation du module scoring.js
-    - génération des valeur de la grille et de leurs attributs
-    - dessin de la grille dans la balise canva à l'ai des des valeurs générées
-    - affichage de la modal de lancemant de partie
-    - ajout de l'event click sur le bouton de lancement de partie au click sur ce bouton:
+    - génération des valeurs de la grille et de leurs attributs
+    - dessin de la grille dans la balise canvas à l'aide des valeurs générées juste avant
+    - affichage de la modale de lancemant de partie
+    - ajout de l'event click sur le bouton de lancement de partie. Au click sur ce bouton:
         - le booléen running passe à true la partie est lancée
-        - on clear l'interval précédant (utile dans le cas ou la partie a été relancée)
+        - on clear l'interval précédant (pour le cas ou la partie a été relancée)
         - on définit la vitesse du jeu à l'aide des datas
         - on définit les valeurs de la pièce courante: (les valeurs de la pièce sont toutes à null puisque la pièce courante n'a pas encore été générée)
             - les positions de la pièce courante reçoit null pour valeur 
             - le type de pièce courante reçoit null pour valeur 
             - la couleur de la pièce courante reçoit null pour valeur  
-        - on cache la modal
+        - on cache la modale
         - on appel la méthode qui lance la partie
 
-- launch_game(), permet de lancer la partie, détailles des instructions:
+- launch_game(), permet de lancer la partie, détail des instructions:
     - on appel la méthode du module piece.js qui permet de récupérer une pièce aléatoirement parmis les 7 possibles
     - on stock notre interval dans la propriété interval ce qui nous permettra de la stopper facilement, l'interval appelera la métode on_move toute les game.speed millisecondes
     - on appel la méthode du module user_inputs.js qui permet d'activer les évènement sur les inputs utilisateur
 
-- on_move(), permet de gérer toute la partie parcourt de la pièce courante:
+- on_move(), permet de gérer tout le parcours de la pièce courante:
     - on a d'abord un flag nommé move, passé par défaut à true et qui détermine si la pièce courante peut se déplacer à true ou pas à false
     - ensuite on a 3 bloques de vérifications qui vont changer ou non notre flag:
         - 1er bloque: check si une des positions de départ de notre pièce est la même qu'une pièce déjà placée:
@@ -171,7 +171,7 @@ Les propriétés vont nous permettre de déterminer un état et les méthodes de
             - on parcourt toutes les position de la pièce courante
             - pour chacune des ces positions on va parcourir toute la grille
             - pour vérifier si il y a un match on va anticiper la valeur qu'aura la positions de la pièce courante lors de son prochain déplacement on peut le traduire comme ceci:
-                - si la position courante de la pièce + une case vers le bas match avec la position de la grille et que la position de la grille est occupée par une pièce déjà placée
+                - si la position courante de la pièce + une case vers le bas match avec la position de la grille et que la position de la grille est occupée par une pièce qui est déjà placée
                 - alors on passe notre flag move à false
                 - et on clear notre interval
     - après avoir fait toutes ces vérifications on va checker si le flag move est à true:
@@ -185,7 +185,7 @@ Les propriétés vont nous permettre de déterminer un état et les méthodes de
             - on appel la méthode set_best_score() du module scoring.js qui mettra à jour le meilleure score en local storage
             - on remet la valeur du booléen game.over à false
             - on appel la méthode remove_key_event() du module user_input.js pour désactiver les inputs utilisateur
-            - on modifie le titre de la modal par Game Over!
+            - on modifie le titre de la modale par Game Over!
             - on appel notre méthode game.init() 
             - la boucle est bouclée le user peut relancer une partie
 
